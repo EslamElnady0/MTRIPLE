6f38931a6ec9ed4c24cf1c962f0368516b71c1ca
EslamElnady0 <eslamelnady254@gmail.com>
2024-02-19 09:44:12 +0200

    firebase added to project

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:mtriple/core/routes/routes.dart';
 import 'package:mtriple/core/themes/themes.dart';
 
 import 'package:mtriple/features/splash/ui/screens/splash_screen.dart';
+import 'package:mtriple/firebase_options.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 int? isViewed;
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   SharedPreferences prefs = await SharedPreferences.getInstance();
 
   isViewed = prefs.getInt('onBoard');
