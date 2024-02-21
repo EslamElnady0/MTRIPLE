405b2a40e9a3d3d5df3907a829c463a9697c90d3
Eslam Elnady <122048669+EslamElnady0@users.noreply.github.com>
2024-02-21 20:51:44 +0200

    Update routes.dart

--- lib/core/Routes/routes.dart
+++ lib/core/Routes/routes.dart
@@ -1,11 +1,20 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:mtriple/features/auth/ui/screens/auth_start_screen.dart';
+import 'package:mtriple/features/auth/ui/screens/forgot_password_screen.dart';
+import 'package:mtriple/features/auth/ui/screens/login_screen.dart';
+import 'package:mtriple/features/auth/ui/screens/nickname_screen.dart';
+import 'package:mtriple/features/auth/ui/screens/signup_screen.dart';
+import 'package:mtriple/features/home/ui/screens/home_screen.dart';
+import 'package:mtriple/features/issue/ui/screens/IssueScreen.dart';
+import 'package:mtriple/features/onBoarding/ui/screens/onboarding_screen.dart';
+import 'package:mtriple/features/relax-breathing/ui/screens/relax_breathing_screen.dart';
+import 'package:mtriple/features/relax/screens/relax_tech_screen.dart';
+import 'package:mtriple/features/splash/ui/screens/splash_screen.dart';
 import 'package:page_transition/page_transition.dart';
 
 import '../../features/Breathing_details/ui/screens/breathPower_screen.dart';
 import '../../features/Profile_screen/screens/profile_screen.dart';
 import '../../features/Setting_Screen/screens/setting_screen.dart';
-import '../../features/relax-breathing/ui/screens/relax-breathing_screen.dart';
 
 class Routes {
   static const String relaxBreathingScreen = 'relax-breathing_screen';
@@ -14,7 +23,15 @@ class Routes {
   static const String setting = 'setting_screen';
   static const String account = 'profile_screen.dart';
   static const String authStart = 'auth-start';
-
+  static const String home = 'home';
+  static const String splash = 'splash';
+  static const String onBoarding = 'on-boarding';
+  static const String login = 'login';
+  static const String signUp = 'sign-up';
+  static const String forgotPassword = 'forgot-password';
+  static const String nickName = 'nickname';
+  static const String issue = 'issue';
+  static const String relax = 'relax';
 
   static Route<dynamic> onGenerateRoutes(RouteSettings settings) {
     switch (settings.name) {
@@ -24,10 +41,36 @@ class Routes {
             type: PageTransitionType.rightToLeft);
       case breath:
         return MaterialPageRoute(builder: (_) => const BreathPowerScreen());
+      case splash:
+        return MaterialPageRoute(builder: (_) => const SplashScreen());
       case setting:
         return MaterialPageRoute(builder: (_) => const SettingScreen());
       case account:
-        return MaterialPageRoute(builder: (_) =>  ProfileScreen());
+        return MaterialPageRoute(builder: (_) => const ProfileScreen());
+      case authStart:
+        return PageTransition(
+            child: const AuthStartScreen(),
+            type: PageTransitionType.rightToLeft);
+      case onBoarding:
+        return MaterialPageRoute(builder: (_) => const OnBoardingScreen());
+      case login:
+        return MaterialPageRoute(builder: (_) => const LogInScreen());
+      case issue:
+        return MaterialPageRoute(builder: (_) => const IssueScreen());
+      case relax:
+        return MaterialPageRoute(builder: (_) => const RelaxTechScreen());
+      case signUp:
+        return MaterialPageRoute(builder: (_) => const SignUpScreen());
+      case forgotPassword:
+        return PageTransition(
+            child: const ForgotPasswordScreen(), type: PageTransitionType.fade);
+      case nickName:
+        return PageTransition(
+            child: const NickNameScreen(),
+            type: PageTransitionType.rightToLeftWithFade);
+      case home:
+        return PageTransition(
+            child: const HomeScreen(), type: PageTransitionType.rightToLeft);
 
       default:
         return MaterialPageRoute(
@@ -36,10 +79,8 @@ class Routes {
   }
 }
 
-
 Future navigateTo(
-    BuildContext context,
-     String routes,
-    ) =>
-    Navigator.pushNamed(context,routes
-         );
+  BuildContext context,
+  String routes,
+) =>
+    Navigator.pushNamed(context, routes);
