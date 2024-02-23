1c7aff466509db48404fe168f57f670e3b7499cb
Eslam Elnady <122048669+EslamElnady0@users.noreply.github.com>
2024-02-23 09:08:47 +0200

    Update routes.dart

--- lib/core/Routes/routes.dart
+++ lib/core/Routes/routes.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:mtriple/features/Profile_screen/screens/edit_post_screen.dart';
 import 'package:mtriple/features/auth/ui/screens/auth_start_screen.dart';
 import 'package:mtriple/features/auth/ui/screens/forgot_password_screen.dart';
 import 'package:mtriple/features/auth/ui/screens/login_screen.dart';
 import 'package:mtriple/features/auth/ui/screens/nickname_screen.dart';
 import 'package:mtriple/features/auth/ui/screens/signup_screen.dart';
+import 'package:mtriple/features/home/ui/screens/add_post_screen.dart';
 import 'package:mtriple/features/home/ui/screens/home_screen.dart';
 import 'package:mtriple/features/issue/ui/screens/IssueScreen.dart';
 import 'package:mtriple/features/onBoarding/ui/screens/onboarding_screen.dart';
@@ -32,6 +34,8 @@ class Routes {
   static const String nickName = 'nickname';
   static const String issue = 'issue';
   static const String relax = 'relax';
+  static const String addPost = 'add-post';
+  static const String editPost = 'edit-post';
 
   static Route<dynamic> onGenerateRoutes(RouteSettings settings) {
     switch (settings.name) {
@@ -46,7 +50,8 @@ class Routes {
       case setting:
         return MaterialPageRoute(builder: (_) => const SettingScreen());
       case account:
-        return MaterialPageRoute(builder: (_) => const ProfileScreen());
+        return PageTransition(
+            child: const ProfileScreen(), type: PageTransitionType.fade);
       case authStart:
         return PageTransition(
             child: const AuthStartScreen(),
@@ -61,6 +66,12 @@ class Routes {
         return MaterialPageRoute(builder: (_) => const RelaxTechScreen());
       case signUp:
         return MaterialPageRoute(builder: (_) => const SignUpScreen());
+      case addPost:
+        return PageTransition(
+            child: const AddPostScreen(), type: PageTransitionType.fade);
+      case editPost:
+        return PageTransition(
+            child: const EditPostScreen(), type: PageTransitionType.fade);
       case forgotPassword:
         return PageTransition(
             child: const ForgotPasswordScreen(), type: PageTransitionType.fade);
